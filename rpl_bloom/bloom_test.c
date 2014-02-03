1d0e1cda515b8bef42cb742ef110b443edb9d641
silke <silke@silke-ubuntu>
2014-02-03 22:45:15 +0100

    * added sets.h generated by C. Mehlis * added base implementation needed for further testing

--- rpl_bloom/bloom_test.c
+++ rpl_bloom/bloom_test.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "hashes.h"
 #include "random.h"
@@ -17,21 +18,47 @@
 #include "include/bloom_test.h"
 #include "include/sets.h"
 
-#define MYSEED 0x83d385c0 /* random number */
+struct bloom_t* create_bloom_filter(uint8_t hash_function_count,
+		uint8_t array_size) {
+	struct bloom_t* bloom;
+	switch (hash_function_count) {
+	case 2:
+		bloom = bloom_new(array_size, hash_function_count, fnv_hash, sax_hash);
+		break;
+	case 3:
+		bloom = bloom_new(array_size, hash_function_count, fnv_hash, sax_hash,
+				sdbm_hash);
+		break;
+	case 4:
+		bloom = bloom_new(array_size, hash_function_count, fnv_hash, sax_hash,
+				sdbm_hash, djb2_hash);
+		break;
+	case 5:
+		bloom = bloom_new(array_size, hash_function_count, fnv_hash, sax_hash,
+				sdbm_hash, djb2_hash, kr_hash);
+		break;
+	case 6:
+		bloom = bloom_new(array_size, hash_function_count, fnv_hash, sax_hash,
+				sdbm_hash, djb2_hash, kr_hash, dek_hash);
+		break;
+	case 7:
+		bloom = bloom_new(array_size, hash_function_count, fnv_hash, sax_hash,
+				sdbm_hash, djb2_hash, kr_hash, dek_hash, rotating_hash);
+		break;
+	case 8:
+		bloom = bloom_new(array_size, hash_function_count, fnv_hash, sax_hash,
+				sdbm_hash, djb2_hash, kr_hash, dek_hash, rotating_hash,
+				one_at_a_time_hash);
+		break;
+	default:
+		continue;
+		break;
+	}
+	return bloom;
+}
 
-// Eingabepuffer enthält immer nur EIN Wort.
-uint32_t* current_element;
-// Enthält alle Wörter
-uint32_t* set;
-// Einfügepuffer enthält ALLE eingefügten Wörter.
-uint32_t* subset;
-// Zeiger, der immer auf die aktuelle Position in BUFFER_INSERTED zeigt.
-uint32_t* insertion_pos;
-
-void test_start(size_t num_hashes, size_t array_size, uint8_t input_length,
-		uint8_t num_words) {
+void test_start(size_t num_hashes, size_t array_size /*, uint8_t num_words*/) {
 	printf("------ test_start ------ \n");
-	printf("------ Hallo!!!!! ------ \n");
 	printf("------ num_hashes: %d ------ \n", num_hashes);
 	// Hashfunktionen durchlaufen
 	for (uint8_t current_hash_nr = 1; current_hash_nr < num_hashes;
@@ -39,59 +66,23 @@ void test_start(size_t num_hashes, size_t array_size, uint8_t input_length,
 		// array size durchlaufen
 		for (uint8_t current_array_size = 2; current_array_size < array_size;
 				current_array_size++) {
-			struct bloom_t *bloom;
 
-			switch (current_hash_nr) {
-			case 2:
-				bloom = bloom_new(current_array_size, current_hash_nr, fnv_hash,
-						sax_hash);
-				break;
-			case 3:
-				bloom = bloom_new(current_array_size, current_hash_nr, fnv_hash,
-						sax_hash, sdbm_hash);
-				break;
-			case 4:
-				bloom = bloom_new(current_array_size, current_hash_nr, fnv_hash,
-						sax_hash, sdbm_hash, djb2_hash);
-				break;
-			case 5:
-				bloom = bloom_new(current_array_size, current_hash_nr, fnv_hash,
-						sax_hash, sdbm_hash, djb2_hash, kr_hash);
-				break;
-			case 6:
-				bloom = bloom_new(current_array_size, current_hash_nr, fnv_hash,
-						sax_hash, sdbm_hash, djb2_hash, kr_hash, dek_hash);
-				break;
-			case 7:
-				bloom = bloom_new(current_array_size, current_hash_nr, fnv_hash,
-						sax_hash, sdbm_hash, djb2_hash, kr_hash, dek_hash,
-						rotating_hash);
-				break;
-			case 8:
-				bloom = bloom_new(current_array_size, current_hash_nr, fnv_hash,
-						sax_hash, sdbm_hash, djb2_hash, kr_hash, dek_hash,
-						rotating_hash, one_at_a_time_hash);
-				break;
-			default:
-				continue;
-				break;
-				//	TODO: default -> Abbrechen und nicht den Test in der nächsten for Schleife starten.
-			}
-			struct bloom_t *bloom = bloom_new(1 << 7, 6, fnv_hash, sax_hash,
-					sdbm_hash, djb2_hash, kr_hash, dek_hash, rotating_hash,
-					one_at_a_time_hash);
+			struct bloom_t* bloom = create_bloom_filter(current_hash_nr, current_array_size);
 
 			printf("Testing Bloom filter.\n\n");
-			printf("m: %zd\nk: %zd\n\n", bloom->m, bloom->k);
+			printf("m: %zd k: %zd\n", bloom->m, bloom->k);
 
 			for (int i = 0; i < lenB; i++) {
 				bloom_add(bloom, (const uint8_t *) B[i], strlen(B[i]));
-				printf("Added \"%s\"\n", B[i]);
+				//printf("Added \"%s\"\n", B[i]);
 			}
 
 			int in = 0;
 			int not_in = 0;
 
+			// Diese Schleife prüft, ob Element aus A im bloom Filter ist.
+			// in => ist in Bloom filter.
+			// not_in => ist NICHT in bloom filter.
 			for (int i = 0; i < lenA; i++) {
 				if (bloom_check(bloom, (const uint8_t *) A[i], strlen(A[i]))) {
 					in++;
@@ -99,44 +90,29 @@ void test_start(size_t num_hashes, size_t array_size, uint8_t input_length,
 					not_in++;
 				}
 			}
-
 			printf("\n");
-			printf("%d elements probably in the filter.\n", in);
-			printf("%d elements not in the filter.\n", not_in);
+			printf("%d elements are NOT in the filter but returned true (false_positive).\n", in);
+			//printf("%d elements are NOT in the filter and returned false.\n", not_in);
 			double false_positive_rate = (double) in / (double) lenA;
 			printf("%f false positive rate.\n", false_positive_rate);
 
+			in = 0;
+			not_in = 0;
+			for (int i = 0; i < lenB; i++) {
+				if (bloom_check(bloom, (const uint8_t *) B[i], strlen(B[i]))) {
+					in++;
+				} else {
+					not_in++;
+				}
+			}
+			double false_negative_rate = (double) not_in / (double) lenB;
+			printf("%d elements are IN the filter but returned false (false_negative).\n", not_in);
+			printf("%f false negative rate.\n", false_negative_rate);
+			assert(not_in == 0);
+
 			bloom_del(bloom);
 			printf("\nAll done!\n");
-
-			// Set pointer to start of buffer.
-			insertion_pos = subset;
-			for (int i = 0; i < num_words; i++) {
-				bool result;
-				result = bloom_check(bloom, (uint8_t*) insertion_pos,
-						input_length * 4);
-				printf("%d\n", result);
-				insertion_pos += input_length;
-			}
-			free(bloom->a);
 		}
 	}
 	printf("fertig!");
-	free(subset);
-	free(current_element);
-}
-
-void fill_buffer(uint32_t* buffer, uint32_t buffer_len) {
-	// Input buffer füllen
-	for (uint32_t i = 0; i < buffer_len; i++) {
-		buffer[i] = genrand_uint32();
-	}
-}
-
-void test_init(uint8_t input_length, uint8_t num_words) {
-	printf("Bloom testing initialized...\n");
-	genrand_init(MYSEED);
-	current_element = (uint32_t*) malloc(input_length * 4);
-	subset = (uint32_t*) malloc(num_words * input_length * 4);
-	printf("inserted_buffer is %d big.", num_words * input_length);
 }
--- rpl_bloom/include/bloom_test.h
+++ rpl_bloom/include/bloom_test.h
@@ -8,12 +8,12 @@
 #ifndef BLOOM_TEST_H_
 #define BLOOM_TEST_H_
 
-void fill_buffer(uint32_t* buffer, uint32_t buffer_len);
+//void fill_buffer(uint32_t* buffer, uint32_t buffer_len);
 /*
  * @brief Initializes all needed buffers, variables etc.
  */
-void test_init(uint8_t input_length, uint8_t num_words);
+//void test_init(uint8_t input_length, uint8_t num_words);
 
-void test_start(size_t num_hashes, size_t array_size, uint8_t input_length, uint8_t num_words);
+void test_start(size_t num_hashes, size_t array_size /*, uint8_t num_words*/);
 
 #endif /* BLOOM_TEST_H_ */
